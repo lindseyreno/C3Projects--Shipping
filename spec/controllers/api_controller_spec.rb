cfb69eefab17aa5d90b0f78baac6f0049720f731
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-08-18 15:04:04 -0700

    added sample serialized json for testing

--- spec/controllers/api_controller_spec.rb
+++ spec/controllers/api_controller_spec.rb
@@ -2,8 +2,10 @@ require 'rails_helper'
 
 RSpec.describe ApiController, type: :controller do
   describe "GET #get_ups_rates" do
+    let(:shipment) {
+      "{\"shipment\":{\"origin\":{\"country\":\"US\",\"state\":\"WA\",\"city\":\"Seattle\",\"zip\":98101},\"destination\":{\"country\":\"US\",\"state\":\"WA\",\"city\":\"Vancouver\",\"zip\":98660},\"packages\":[{\"weight\":100,\"dimensions\":[12,12,12]}]}}" }
     before :each do
-      get :get_ups_rates
+      get :get_ups_rates, shipment: shipment
     end
 
     it "is successful" do
@@ -16,3 +18,8 @@ RSpec.describe ApiController, type: :controller do
   end
 
 end
+
+# {:shipment=>
+#   {:origin=>{:country=>"US", :state=>"WA", :city=>"Seattle", :zip=>98101},
+#    :destination=>{:country=>"US", :state=>"WA", :city=>"Vancouver", :zip=>98660},
+#    :packages=>[{:weight=>100, :dimensions=>[12, 12, 12]}]}}
