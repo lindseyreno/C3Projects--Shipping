6a2c4d94caca4d945cf2bdb00992e9a12ef20bc9
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-08-18 15:04:23 -0700

    added require ups_interface

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
+require 'ups_interface'
+
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
