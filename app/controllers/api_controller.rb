ed8ab137481c970472ce9aa4b524244ca7db65d3
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-08-18 13:30:39 -0700

    created ApiController with #get_ups_rates and ORIGIN constant

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,2 +1,14 @@
 class ApiController < ApplicationController
+  ORIGIN = ActiveShipping::Location.new(
+    country: 'US',
+    state: 'WA',
+    city: 'Seattle',
+    zip: '98101'
+    )
+
+  def get_ups_rates
+    ups = UpsInterface.new.ups
+
+    ups.find_rates(ORIGIN, destination, packages)
+  end
 end
--- spec/controllers/api_controller_spec.rb
+++ spec/controllers/api_controller_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe ApiController, type: :controller do
+  describe "GET #get_ups_rates" do
+    before :each do
+      get :get_ups_rates
+    end
+
+    it "is successful" do
+      expect(response.response_code).to eq 200
+    end
+
+    # it "returns ups rates" do
+    #   expect(response.header['Content-Type']).to include 'application/json'
+    # end
+  end
 
 end
