bd6c8fd4a38dda3b298b653fb560036ab1012bd5
Lindsey Reno <lindseyannereno@gmail.com>
2015-08-18 15:53:26 -0700

    Added vcr and changed shipment object.

--- spec/controllers/api_controller_spec.rb
+++ spec/controllers/api_controller_spec.rb
@@ -1,15 +1,19 @@
 require 'rails_helper'
+require 'support/vcr_setup'
 
 RSpec.describe ApiController, type: :controller do
   describe "GET #get_ups_rates" do
     let(:shipment) {
-      "{\"shipment\":{\"origin\":{\"country\":\"US\",\"state\":\"WA\",\"city\":\"Seattle\",\"zip\":98101},\"destination\":{\"country\":\"US\",\"state\":\"WA\",\"city\":\"Vancouver\",\"zip\":98660},\"packages\":[{\"weight\":100,\"dimensions\":[12,12,12]}]}}" }
+      # "{\"shipment\":{\"origin\":{\"country\":\"US\",\"state\":\"WA\",\"city\":\"Seattle\",\"zip\":98101},\"destination\":{\"country\":\"US\",\"state\":\"WA\",\"city\":\"Vancouver\",\"zip\":98660},\"packages\":[{\"weight\":100,\"dimensions\":[12,12,12]}]}}" }
+      "{\"shipment\":{\"origin\":{\"country\":\"US\",\"state\":\"WA\",\"city\":\"Seattle\",\"zip\":98101},\"destination\":{\"country\":\"US\",\"state\":\"CA\",\"city\":\"San Leandro\",\"zip\":94578},\"packages\":[{\"weight\":100,\"dimensions\":[12,12,12]}]}}" }   
     before :each do
-      get :get_ups_rates, shipment: shipment
+      VCR.use_cassette "controllers/get_ups_rates" do
+        @response = get :get_ups_rates, shipment: shipment
+      end
     end
 
     it "is successful" do
-      expect(response.response_code).to eq 200
+      expect(@response.response_code).to eq 200
     end
 
     # it "returns ups rates" do
@@ -19,7 +23,4 @@ RSpec.describe ApiController, type: :controller do
 
 end
 
-# {:shipment=>
-#   {:origin=>{:country=>"US", :state=>"WA", :city=>"Seattle", :zip=>98101},
-#    :destination=>{:country=>"US", :state=>"WA", :city=>"Vancouver", :zip=>98660},
-#    :packages=>[{:weight=>100, :dimensions=>[12, 12, 12]}]}}
+# {:shipment=> {:origin=>{:country=>"US", :state=>"WA", :city=>"Seattle", :zip=>98101}, :destination=>{:country=>"US", :state=>"CA", :city=>"San Leandro", :zip=>94578}, :packages=>[{:weight=>100, :dimensions=>[12, 12, 12]}]}}
