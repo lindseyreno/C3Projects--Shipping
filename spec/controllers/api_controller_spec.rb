78173d82816ccdfdff6f58563d1aa0023ea8fefe
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-08-18 16:21:48 -0700

    added spec for returning multiple rates

--- spec/controllers/api_controller_spec.rb
+++ spec/controllers/api_controller_spec.rb
@@ -4,7 +4,6 @@ require 'support/vcr_setup'
 RSpec.describe ApiController, type: :controller do
   describe "GET #get_ups_rates" do
     let(:shipment) {
-      # "{\"shipment\":{\"origin\":{\"country\":\"US\",\"state\":\"WA\",\"city\":\"Seattle\",\"zip\":98101},\"destination\":{\"country\":\"US\",\"state\":\"WA\",\"city\":\"Vancouver\",\"zip\":98660},\"packages\":[{\"weight\":100,\"dimensions\":[12,12,12]}]}}" }
       "{\"shipment\":{\"origin\":{\"country\":\"US\",\"state\":\"WA\",\"city\":\"Seattle\",\"zip\":98101},\"destination\":{\"country\":\"US\",\"state\":\"CA\",\"city\":\"San Leandro\",\"zip\":94578},\"packages\":[{\"weight\":100,\"dimensions\":[12,12,12]}]}}" }   
     before :each do
       VCR.use_cassette "controllers/get_ups_rates" do
@@ -32,10 +31,16 @@ RSpec.describe ApiController, type: :controller do
       it "is an array" do
         expect(@rates).to be_an_instance_of Array
       end
+
+      it "returns multiple rates" do
+        sorted_rates = @rates.sort_by {|rate| rate["total_price"]}.collect do |rate|
+          [rate["service_name"], rate["total_price"]]
+        end
+        expect(sorted_rates.first.first).to eq "UPS Ground"
+      end
     end
-
   end
-
 end
 
+# Use the below code for creating test JSON for specs
 # {:shipment=> {:origin=>{:country=>"US", :state=>"WA", :city=>"Seattle", :zip=>98101}, :destination=>{:country=>"US", :state=>"CA", :city=>"San Leandro", :zip=>94578}, :packages=>[{:weight=>100, :dimensions=>[12, 12, 12]}]}}
