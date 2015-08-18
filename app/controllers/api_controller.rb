2061d7c4e592df6312199adf743702e9dfb65fd0
Lindsey Reno <lindseyannereno@gmail.com>
2015-08-18 15:51:02 -0700

    Fixed bug in location objects.

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,10 +1,9 @@
 class ApiController < ApplicationController
   def get_ups_rates
     ups = UpsInterface.new.ups
-    shipment = JSON.parse(params[:shipment])
+    shipment = JSON.parse(params[:shipment])["shipment"]
     raw_packages = shipment["packages"]
 
-    binding.pry
     packages = []
     raw_packages.each do |package|
       new_package = ActiveShipping::Package.new(
@@ -12,9 +11,19 @@ class ApiController < ApplicationController
       packages << new_package
     end
 
-    origin = ActiveShipping::Location.new(shipment["origin"])
+    origin = ActiveShipping::Location.new(
+      country: shipment["origin"]["country"],
+      state: shipment["origin"]["state"],
+      city: shipment["origin"]["city"],
+      zip: shipment["origin"]["zip"]
+      )
 
-    destination = ActiveShipping::Location.new(shipment["destination"])
+    destination = ActiveShipping::Location.new(
+      country: shipment["origin"]["country"],
+      state: shipment["origin"]["state"],
+      city: shipment["origin"]["city"],
+      zip: shipment["origin"]["zip"]
+      )
 
     response = ups.find_rates(origin, destination, packages)
 
