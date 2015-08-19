1de9471c860de09dc24547a7b461091faeab220b
Lindsey Reno <lindseyannereno@gmail.com>
2015-08-19 11:07:12 -0700

    Moved UPS processing to lib/ups_interface.rb

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,49 +1,10 @@
 class ApiController < ApplicationController
-  DESIRED_UPS_RATES = ["UPS Ground", "UPS Second Day Air", "UPS Next Day Air"]
   DESIRED_USPS_RATES = ["USPS First-Class Mail Parcel", "USPS Priority Mail 1-Day"]
 
   def get_ups_rates
-    ups = UpsInterface.new.ups
     shipment = JSON.parse(params[:shipment])["shipment"]
-    raw_packages = shipment["packages"]
-
-    packages = []
-    raw_packages.each do |package|
-      new_package = ActiveShipping::Package.new(
-        package["weight"], package["dimensions"])
-      packages << new_package
-    end
-
-    origin = ActiveShipping::Location.new(
-      country: shipment["origin"]["country"],
-      state: shipment["origin"]["state"],
-      city: shipment["origin"]["city"],
-      zip: shipment["origin"]["zip"]
-      )
-
-    destination = ActiveShipping::Location.new(
-      country: shipment["origin"]["country"],
-      state: shipment["origin"]["state"],
-      city: shipment["origin"]["city"],
-      zip: shipment["origin"]["zip"]
-      )
-
-    response = ups.find_rates(origin, destination, packages)
-    rates = response.rates
-
-    ## return only the desired rates
-    collected_rates = []
-    rates.each do |rate|
-      collected_rates << rate if DESIRED_UPS_RATES.include?(rate.service_name)
-    end
-
-    ## pull out just the service_name and calculate the total rate
-    rate_price_pairs = []
-    collected_rates.each do |rate|
-      rate_price_pairs << {
-        service_name: rate.service_name, total_price: rate.total_price
-      }
-    end
+    ups = UpsInterface.new
+    rate_price_pairs = ups.process_ups_rates(shipment)
 
     render json: rate_price_pairs
   end
@@ -96,5 +57,13 @@ class ApiController < ApplicationController
     end
 
     render json: rate_price_pairs
+  end
+
+  def get_all_rates
+    ups_rates = get_ups_rates
+    usps_rates = get_usps_rates
+    all_rates = ups_rates + usps_rates
+
+    render json: all_rates
   end
 end
