3311bfc4008e2b91aed51cafaa48a760d4d3fa72
Lindsey Reno <lindseyannereno@gmail.com>
2015-08-19 11:32:04 -0700

    Moved USPS processing to lib/usps_interface.rb; added usps rates to get_all_rates

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,60 +1,16 @@
 class ApiController < ApplicationController
-  DESIRED_USPS_RATES = ["USPS First-Class Mail Parcel", "USPS Priority Mail 1-Day"]
-
   def get_ups_rates
-    shipment = JSON.parse(params[:shipment])["shipment"]
     ups = UpsInterface.new
+    shipment = JSON.parse(params[:shipment])["shipment"]
     rate_price_pairs = ups.process_rates(shipment)
 
     render json: rate_price_pairs
   end
 
   def get_usps_rates
-    usps = UspsInterface.new.usps
+    usps = UspsInterface.new
     shipment = JSON.parse(params[:shipment])["shipment"]
-    raw_packages = shipment["packages"]
-
-    packages = []
-    raw_packages.each do |package|
-      new_package = ActiveShipping::Package.new(
-        package["weight"], package["dimensions"])
-      packages << new_package
-    end
-
-    origin = ActiveShipping::Location.new(
-      country: shipment["origin"]["country"],
-      state: shipment["origin"]["state"],
-      city: shipment["origin"]["city"],
-      zip: shipment["origin"]["zip"]
-      )
-
-    destination = ActiveShipping::Location.new(
-      country: shipment["origin"]["country"],
-      state: shipment["origin"]["state"],
-      city: shipment["origin"]["city"],
-      zip: shipment["origin"]["zip"]
-      )
-
-    response = usps.find_rates(origin, destination, packages)
-    rates = response.rates
-
-    ## collect only the desired service rates
-    collected_rates = []
-    rates.each do |rate|
-      collected_rates << rate if DESIRED_USPS_RATES.include?(rate.service_name)
-    end
-
-    ## pull out just the service_name and calculate the total rate
-    rate_price_pairs = []
-    collected_rates.each do |rate|
-      collected_prices = 0
-      rate.package_rates.each do |package_rate|
-        collected_prices += package_rate[:rate]
-      end
-      rate_price_pairs << {
-        service_name: rate.service_name, total_price: collected_prices
-      }
-    end
+    rate_price_pairs = usps.process_rates(shipment)
 
     render json: rate_price_pairs
   end
@@ -62,9 +18,9 @@ class ApiController < ApplicationController
   def get_all_rates
     shipment = JSON.parse(params[:shipment])["shipment"]
     ups_rates = UpsInterface.new.process_rates(shipment)
-    # usps_rates = get_usps_rates
-    # all_rates = ups_rates + usps_rates
+    usps_rates = UspsInterface.new.process_rates(shipment)
+    all_rates = ups_rates + usps_rates
 
-    render json: ups_rates
+    render json: all_rates
   end
 end
