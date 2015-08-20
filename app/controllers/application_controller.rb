6eb973e4658288a053f9c8caf35308badd6e6852
Lindsey Reno <lindseyannereno@gmail.com>
2015-08-19 19:45:57 -0700

    Required shipping_interface

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ require 'active_shipping'
 # Our classes for interacting with shippers
 require 'ups_interface'
 require 'usps_interface'
+require 'shipping_interface'
 
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
