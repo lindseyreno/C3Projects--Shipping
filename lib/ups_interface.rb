1f89fc282ef672f4ff642ce83b5c6e7734435276
Lindsey Reno <lindseyannereno@gmail.com>
2015-08-19 19:47:48 -0700

    Refactored using shipping_interface.rb; added delivery_date.

--- lib/ups_interface.rb
+++ lib/ups_interface.rb
@@ -1,6 +1,6 @@
 class UpsInterface
   attr_reader :ups
-  DESIRED_UPS_RATES = ["UPS Ground", "UPS Second Day Air", "UPS Next Day Air"]
+  DESIRED_SERVICES = ["UPS Ground", "UPS Second Day Air", "UPS Next Day Air"]
 
   def initialize
     @ups = ActiveShipping::UPS.new(
@@ -11,46 +11,27 @@ class UpsInterface
   end
 
   def process_rates(shipment)
-    raw_packages = shipment["packages"]
+    packages = ShippingInterface.create_packages(shipment["packages"])
+    origin = ShippingInterface.create_location(shipment["origin"])
+    destination = ShippingInterface.create_location(shipment["destination"])
 
-    packages = []
-    raw_packages.each do |package|
-      new_package = ActiveShipping::Package.new(
-        package["weight"], package["dimensions"])
-      packages << new_package
-    end
-
-    origin = ActiveShipping::Location.new(
-      country: shipment["origin"]["country"],
-      state: shipment["origin"]["state"],
-      city: shipment["origin"]["city"],
-      zip: shipment["origin"]["zip"]
-      )
-
-    destination = ActiveShipping::Location.new(
-      country: shipment["origin"]["country"],
-      state: shipment["origin"]["state"],
-      city: shipment["origin"]["city"],
-      zip: shipment["origin"]["zip"]
-      )
-
-    response = ups.find_rates(origin, destination, packages)
+    response = ups.find_rates(origin, destination, packages, {pickup_time: Date.current})
     rates = response.rates
 
     ## return only the desired rates
     collected_rates = []
     rates.each do |rate|
-      collected_rates << rate if DESIRED_UPS_RATES.include?(rate.service_name)
+      collected_rates << rate if DESIRED_SERVICES.include?(rate.service_name)
     end
 
     ## pull out just the service_name and calculate the total rate
-    rate_price_pairs = []
+    rate_info = []
     collected_rates.each do |rate|
-      rate_price_pairs << {
-        service_name: rate.service_name, total_price: rate.total_price
+      rate_info << {
+        service_name: rate.service_name, total_price: rate.total_price, delivery_date: rate.delivery_date
       }
     end
-    return rate_price_pairs
+    return rate_info
   end
 end
 
