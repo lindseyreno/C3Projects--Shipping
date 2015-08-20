a46634c3721ea4b7113618bb39fdc1cbac577b16
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-08-20 15:41:03 -0700

    added error handling for bad parameters in shipping_choice in #log_shipping_choice

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,4 +1,11 @@
 class ApiController < ApplicationController
+  BAD_PARAM_ERROR_MESSAGE = { error_details: 
+        %Q{The shipping choice is missing a parameter 
+        or a parameter is in the wrong format:
+        shipping_service(string), 
+        shipping_cost(integer),
+        and order_id(integer).} }
+
   def get_ups_rates
     ups = UpsInterface.new
     shipment = JSON.parse(params[:json_data])["shipment"]
@@ -28,9 +35,9 @@ class ApiController < ApplicationController
     shipping_choice = JSON.parse(params[:json_data])["shipping_choice"]
     audit = Audit.new(shipping_choice)
     if audit.save
-      render json: {}, status: 201
+      render json: Audit.find(audit.id), status: 201
     elsif audit.errors
-      render json: {}, status: 422
+      render json: BAD_PARAM_ERROR_MESSAGE, status: 422
     else
       render json: {}, status: 400
     end
