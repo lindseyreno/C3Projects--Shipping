48f5213d9c1480ef2286a73f4b4835b1803e4cfc
Lindsey Reno <lindseyannereno@gmail.com>
2015-08-20 14:31:48 -0700

    Added route for log_shipping_choice

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get '/get_ups_rates', to: 'api#get_ups_rates'
   get '/get_usps_rates', to: 'api#get_usps_rates'
   get '/get_all_rates', to: 'api#get_all_rates'
+  post '/log_shipping_choice', to: 'api#log_shipping_choice'
 end
