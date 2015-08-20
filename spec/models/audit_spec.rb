872c218e8cdf5d2f5e2ac76092226b9b687f0325
Lindsey Reno <lindseyannereno@gmail.com>
2015-08-20 13:50:22 -0700

    Added tests for validations for shipping_service presence and shipping_cost presence and numericality

--- spec/models/audit_spec.rb
+++ spec/models/audit_spec.rb
@@ -22,5 +22,33 @@ RSpec.describe Audit, type: :model do
         expect(audit.errors.keys).to include(:order_id)
       end
     end
+
+    context "shipping_service" do
+      it "requires a shipping_service" do
+        audit.shipping_service = nil
+        audit.valid?
+        expect(audit.errors.keys).to include(:shipping_service)
+      end
+    end
+
+    context "shipping cost" do
+      it "requires a shipping_cost" do
+        audit.shipping_cost = nil
+        audit.valid?
+        expect(audit.errors.keys).to include(:shipping_cost)
+      end
+
+      it "must be a number" do
+        audit.shipping_cost = "a"
+        audit.valid?
+        expect(audit.errors.keys).to include(:shipping_cost)
+      end
+
+      it "must be an integer" do
+        audit.shipping_cost = 23.12
+        audit.valid?
+        expect(audit.errors.keys).to include(:shipping_cost)
+      end
+    end
   end
 end
