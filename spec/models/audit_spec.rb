d110865d03a512049add92c50159f221c02405ad
Lindsey Reno <lindseyannereno@gmail.com>
2015-08-20 13:42:35 -0700

    Added tests for validations of order_id

--- spec/models/audit_spec.rb
+++ spec/models/audit_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Audit, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "model validations" do
+    let(:audit) { build :audit }
+    context "order_id" do
+      it "requires an order_id" do
+        audit.order_id = nil
+        audit.valid?
+        expect(audit.errors.keys).to include(:order_id)
+      end
+
+      it "must be a number" do
+        audit.order_id = "a"
+        audit.valid?
+        expect(audit.errors.keys).to include(:order_id)
+      end
+
+      it "must be an integer" do
+        audit.order_id = 3.2
+        audit.valid?
+        expect(audit.errors.keys).to include(:order_id)
+      end
+    end
+  end
 end
