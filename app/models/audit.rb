8b4a94d22be8a385f37304b2df3a9a30634cbeba
Lindsey Reno <lindseyannereno@gmail.com>
2015-08-20 13:49:46 -0700

    Added validations for shipping_service presence and shipping_cost presence and numericality

--- app/models/audit.rb
+++ app/models/audit.rb
@@ -1,4 +1,6 @@
 class Audit < ActiveRecord::Base
   # Validations
   validates :order_id, presence: true, numericality: { only_integer: true }
+  validates :shipping_service, presence: true
+  validates :shipping_cost, presence: true, numericality: { only_integer: true }
 end
