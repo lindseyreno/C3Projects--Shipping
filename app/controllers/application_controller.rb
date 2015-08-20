6993294a77073d2d93be27f6460e56dd77b708f6
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-08-20 16:04:02 -0700

    commented out protect_from_forgery to allow posts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,5 +8,5 @@ require 'shipping_interface'
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
 end
