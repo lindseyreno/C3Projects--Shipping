9802bd4a659acd9cc24c027793c4a176e39305f3
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-08-21 09:52:53 -0700

    added checks for invalid shipment details to #create_packages and #create_location

--- lib/shipping_interface.rb
+++ lib/shipping_interface.rb
@@ -2,6 +2,7 @@ class ShippingInterface
   def self.create_packages(raw_packages)
     packages = []
     raw_packages.each do |package|
+      return false if package.values.any? { |value| value.nil? } || package["dimensions"].length < 3
       new_package = ActiveShipping::Package.new(
         package["weight"], package["dimensions"])
       packages << new_package
@@ -10,6 +11,7 @@ class ShippingInterface
   end
 
   def self.create_location(location)
+    return false if location.values.any? { |value| value.nil? }
     ActiveShipping::Location.new(
       country: location["country"],
       state: location["state"],
