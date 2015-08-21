e636116407d3f06e75513b1c51097a88838f7b1d
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-08-21 09:52:09 -0700

    added logic to #get_all_rates to check for invalid shipment details before making external API calls

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,10 +1,7 @@
 class ApiController < ApplicationController
   BAD_PARAM_ERROR_MESSAGE = { error_details:
         %Q{The shipping choice is missing a parameter
-        or a parameter is in the wrong format:
-        shipping_service(string),
-        shipping_cost(integer),
-        and order_id(integer).} }
+        or a parameter is in the wrong format.} }
 
   def get_ups_rates
     ups = UpsInterface.new
@@ -24,14 +21,17 @@ class ApiController < ApplicationController
 
   def get_all_rates
     shipment = JSON.parse(params[:json_data])["shipment"]
+
     ups_rates = UpsInterface.new.process_rates(shipment)
     usps_rates = UspsInterface.new.process_rates(shipment)
-    all_rates = ups_rates + usps_rates
-    # if
+
+    all_rates = ups_rates + usps_rates if ups_rates && usps_rates
+
+    if all_rates
       render json: all_rates, status: 200
-    # elsif
-      # render json: BAD_PARAM_ERROR_MESSAGE, status: 422
-    # end
+    else
+      render json: BAD_PARAM_ERROR_MESSAGE, status: 422
+    end
   end
 
   def log_shipping_choice
